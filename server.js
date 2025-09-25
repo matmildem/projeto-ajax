5218d3cfa34afd68dd01a2e56e79a12ad8ecf800
matmildem <matmildem1706@gmail.com>
2025-09-25 11:39:05 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const bodyParser= require("body-parser")
+const express = require("express")
+const app = express()
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({
+    extended: true
+
+}))
+app.use(bodyParser.json())
+
+const multer = require("multer")
+const storage = multer.diskStorage({
+    destination: function(req, file, callback){
+        callback(null, './upload')
+    
+    },
+    filename:function(req, file, callback){
+        callback(null, '${Date.now()}_${file.originalname}')
+    }
+})
+const upload = multer({storage}).single('arquivo')
+app.post('/upload', (req,res)=>{
+    upload(rec,res,err =>{
+        if(err){
+            return res.end('ocorreu um erro')
+        }
+        res.end('concluido com sucesso')
+    })
+})
+app.listen(8080, () =>
+console.log('executando...')
+)
